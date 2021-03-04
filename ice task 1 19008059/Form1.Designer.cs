5016a0139759193eba531e171ee2af3ddfda718a
gseps <77698178+gseps@users.noreply.github.com>
2021-03-04 21:49:27 +0200

    completed ice task

--- ice task 1 19008059/Form1.Designer.cs
+++ ice task 1 19008059/Form1.Designer.cs
@@ -30,32 +30,46 @@ namespace ice_task_1_19008059
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.lblTextChange = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(328, 92);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(159, 35);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Change Label Text";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTextChange
+            // 
+            this.lblTextChange.AutoSize = true;
+            this.lblTextChange.Location = new System.Drawing.Point(343, 38);
+            this.lblTextChange.Name = "lblTextChange";
+            this.lblTextChange.Size = new System.Drawing.Size(35, 13);
+            this.lblTextChange.TabIndex = 1;
+            this.lblTextChange.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1025, 561);
+            this.Controls.Add(this.lblTextChange);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTextChange;
     }
 }
 
--- ice task 1 19008059/Form1.cs
+++ ice task 1 19008059/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace ice_task_1_19008059
 {
@@ -15,6 +16,12 @@ namespace ice_task_1_19008059
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = Interaction.InputBox("Enter Label Name", "Lable Changer", "", 100, 100);
+            lblTextChange.Text = input.ToString();
         }
     }
 }
